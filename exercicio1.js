12dbdc86e5408028a24b52e00a5fd134a737e01a
tiago0022 <tiago.araujo.neo@gmail.com>
2021-06-21 00:57:41 -0300

    fim

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,13 @@
-// Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+const botaoCalcular = document.querySelector('#calcular');
+const constante = document.querySelector('#constante');
+const massa1 = document.querySelector('#massa1');
+const massa2 = document.querySelector('#massa2');
+const distancia = document.querySelector('#distancia');
+const fGrav = document.querySelector('#resultado');
+
+botaoCalcular.addEventListener('click', function () {
+    const numerador = constante.value * massa1.value * massa2.value
+    const denominador = distancia.value ** 2
+    const forcaGravitacional = numerador / denominador
+    fGrav.value = forcaGravitacional
+});
--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,9 @@
-// Faça o exercício dos PARÁGRAFOS aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+botoesExpandir = document.querySelectorAll('.botao-expandir-retrair');
+
+
+botoesExpandir.forEach(function (botao) {
+    botao.addEventListener('click', function (evento) {
+        const expandiu = evento.currentTarget.parentNode.classList.toggle('expandido');
+        evento.currentTarget.innerHTML = expandiu ? '-' : '+';
+    });
+});
